f416539833defca80d9004a9f8bad01d2c8c9cf7
clement-massit <clement.massit@gmail.com>
2021-06-07 23:04:05 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String[] args) throws FileNotFoundException {
 		
+		InterfaceHuffman interface_huffman = new InterfaceHuffman();
+		
+		/*HuffmanTree arbre = new HuffmanTree();
 
-		HuffmanTree arbre = new HuffmanTree();
-
-		File file = new File("test/exemple_freq.txt");
+		File file = new File("test/infinitif freq.txt");
 		HuffmanFile freq_file = new HuffmanFile(file);
 
+		System.out.println(freq_file.Freq_File_Reader());
 		arbre.create_tree(freq_file.transform_to_list_node(freq_file.Freq_File_Reader()));
 		
 
-		File file_to_decompress = new File("test/exemple_comp.txt");
+		File file_to_decompress = new File("test/infinitif.txt");
 		HuffmanFile file_decompress = new HuffmanFile(file_to_decompress);
 		
-		String text_decompressed = HuffmanDecompress.decompress(file_decompress.getBinaryChain(), arbre);
-		HuffmanDecompress.write_text_decompressed(text_decompressed);
 		
+		String text_decompressed = HuffmanDecompress.decompress(file_decompress.getBinaryChain(), arbre);
+		HuffmanDecompress.write_text_decompressed(text_decompressed);*/
 	}
 
 }
